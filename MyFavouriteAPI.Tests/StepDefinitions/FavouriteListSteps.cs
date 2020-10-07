08095cf3e1d761159afbbffe2906eabf2c11b221
@willhead <60652390+will-head@users.noreply.github.com>
2020-10-07 13:53:38 +0100

    Adds FavouriteListSteps Class

---
+++ MyFavouriteAPI.Tests/StepDefinitions/FavouriteListSteps.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MyFavouriteAPI.Tests.StepDefinitions
+{
+    [Binding]
+    public class FavouriteListSteps
+    {
+        [When(@"I create a new favourite list")]
+        public void WhenICreateANewFavouriteList()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the favourite list should be empty")]
+        public void ThenTheFavouriteListShouldBeEmpty()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
