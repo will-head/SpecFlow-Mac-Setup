bb26381a8f7921595675bf16ae5ac279041c1ea4
@willhead <60652390+will-head@users.noreply.github.com>
2020-10-08 14:53:04 +0100

    Adds basic Step Definitions

---
+++ MyFavouriteAPI.Tests/StepDefinitions/FavouriteListSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace MyFavouriteAPI.Tests.StepDefinitions
+{
+    [Binding]
+    public class FavouriteListSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public FavouriteListSteps(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+        [When(@"I create a new favourite list")]
+        public void WhenICreateANewFavouriteList()
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Then(@"the favourite list should be created as empty")]
+        public void ThenTheFavouriteListShouldBeCreatedAsEmpty()
+        {
+            _scenarioContext.Pending();
+        }
+    }
+}
